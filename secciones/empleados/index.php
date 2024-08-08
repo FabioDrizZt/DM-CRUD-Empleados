cd966cb579f60539b8d83b1c0cf3542c8b8d04d3
fabio <ing.fabio.arg@gmail.com>
2024-08-07 23:04:34 -0300

    first commit

---
+++ secciones/empleados/index.php
@@ -0,0 +1,3 @@
+<?php require_once('../../templates/header.php') ;?>
+Listar empleados
+<?php require_once('../../templates/footer.php') ;?>
---
+++ secciones/puestos/index.php
@@ -0,0 +1,4 @@
+<?php require_once('../../templates/header.php') ;?>
+Listar puestos
+
+<?php require_once('../../templates/footer.php') ;?>
