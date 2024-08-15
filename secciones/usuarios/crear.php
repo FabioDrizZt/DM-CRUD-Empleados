2ee70bea6e51425eb740720f69025f9f1a9fc530
Fabio <Fabio@ada-school.com>
2024-08-14 22:33:34 -0300

    [ADD] Index y creación de usuarios

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,3 +1,29 @@
 <?php require_once('../../templates/header.php') ;?>
-Crear empleados
+<div class="card">
+    <div class="card-header">Datos del usuario</div>
+    <div class="card-body">
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="usuario" class="form-label">Usuario:</label>
+                <input type="text" class="form-control" name="" id="" aria-describedby="helpId"
+                    placeholder="Ingrese su nombre de usuario" />
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Contraseña:</label>
+                <input type="password" class="form-control" name="password" id="password"
+                    placeholder="Ingrese su contraseña" />
+            </div>
+            <div class="mb-3">
+                <label for="correo" class="form-label">Correo:</label>
+                <input type="email" class="form-control" name="correo" id="correo" aria-describedby="emailHelpId"
+                    placeholder="abc@mail.com" />
+            </div>
+            <a name="" id="" class="btn btn-outline-danger" href="index.php" role="button">Cancelar</a>
+
+            <button type="submit" class="btn btn-primary">
+                Enviar
+            </button>
+        </form>
+    </div>
+</div>
 <?php require_once('../../templates/footer.php') ;?>
--- secciones/usuarios/index.php
+++ secciones/usuarios/index.php
@@ -1,4 +1,38 @@
 <?php require_once('../../templates/header.php') ;?>
-Listar usuarios
+<div class="card">
+    <div class="card-header">
+        <h1>Usuarios</h1>
+        <a name="" id="" class="btn btn-success" href="crear.php" role="button">Agregar registro</a>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table ">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Usuario</th>
+                        <th scope="col">Contraseña</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <tr class="">
+                        <td scope="row">1</td>
+                        <td>juanPerez</td>
+                        <td>*******</td>
+                        <td>juan.perez@gmail.com</td>
+                        <td>
+                            <a name="" id="" class="btn btn-outline-info" href="editar.php" role="button">Editar🖊</a>
+                            <a name="" id="" class="btn btn-outline-danger" href="#" role="button">Eliminar❌</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
 
 <?php require_once('../../templates/footer.php') ;?>
