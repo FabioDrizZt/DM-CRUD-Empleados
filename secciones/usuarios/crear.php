4ea142c780c423e7e1ac018a9653ddfa2267e73e
Fabio <Fabio@ada-school.com>
2024-08-21 22:09:51 -0300

    [ADD} inserción de usuarios

--- secciones/usuarios/crear.php
+++ secciones/usuarios/crear.php
@@ -1,3 +1,22 @@
+<?php require_once('../../bd.php') ;
+if($_POST){
+    // print_r($_POST);
+    // Recolectar los datos del metodo POST
+    $usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : "";
+    $password = (isset($_POST['password'])) ? $_POST['password'] : "";
+    $correo = (isset($_POST['correo'])) ? $_POST['correo'] : "";
+    // Preparar la inserción de los datos
+    $sentencia = $conexion->prepare("INSERT INTO `tbl_usuarios`(`usuario`, `password`, `correo`) VALUES (:usuario, :password, :correo)");
+    // Asignar valores que vienen del formulario
+    $sentencia->bindParam(":usuario", $usuario);
+    $sentencia->bindParam(":password", $password);
+    $sentencia->bindParam(":correo", $correo);
+    // Ejecutar la sentencia
+    $sentencia->execute();
+    // Redirigir al index, una vez creado el nuevo puesto
+    header("Location: index.php");
+}
+?>
 <?php require_once('../../templates/header.php') ;?>
 <div class="card">
     <div class="card-header">Datos del usuario</div>
@@ -5,7 +24,7 @@
         <form action="" method="post">
             <div class="mb-3">
                 <label for="usuario" class="form-label">Usuario:</label>
-                <input type="text" class="form-control" name="" id="" aria-describedby="helpId"
+                <input type="text" class="form-control" name="usuario" id="usuario" aria-describedby="helpId"
                     placeholder="Ingrese su nombre de usuario" />
             </div>
             <div class="mb-3">
